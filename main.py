db9f89d03a2ac45ac2c90a9d6437f84ed3f40e29
Duke-Axer <duke.axer@gmail.com>
2023-02-06 11:41:04 +0100

    Update main.py
    zacząłem tworzenie funkcji modifyFiles()

--- main.py
+++ main.py
@@ -4,20 +4,8 @@ import os
 
 
 
-<<<<<<< HEAD
 def menu(path):
     path = path + chr(92) + "Data"
-=======
-
-
-def menu():
-    clearCmd()
-    print("Menu\n"
-          "1 --> Wczytanie\n"
-          "2 --> Modyfikacja\n"
-          "0 --> Exit")
-
->>>>>>> 1276d9168cde6062162e9d0560127d1660c0f87e
     while True:
         clearCmd()
         print("Menu\n"
@@ -71,10 +59,7 @@ def openDict():
     pass
 # Dodawanie, usuwanie, zmieniane nazwy folderów
 def modifyDict():
-<<<<<<< HEAD
     path = os.getcwd()
-=======
->>>>>>> 1276d9168cde6062162e9d0560127d1660c0f87e
     next_ = True
     while next_:
         clearCmd()
@@ -90,7 +75,6 @@ def modifyDict():
               "-3 -> Usuń\n"
               "0 --> Cofnij")
         inputMenu = input()
-<<<<<<< HEAD
         if not intiger(inputMenu)[1]:
             while not intiger(inputMenu)[1]:
                 print("Opcja: " + inputMenu + " nie istnieje, proszę wybrać ponownie\n")
@@ -212,100 +196,6 @@ def modifyFiles(path):
                  print("Opcja: " + inputMenu + " nie istnieje, proszę wybrać ponownie\n")
                  inputMenu = input()
 
-=======
-        try:
-            if int(inputMenu) <= len(listDir) and int(inputMenu) > 0:
-                print( "Wybrano folder: " + str(listDir[int(inputMenu)]) )
-
-            elif inputMenu == "-1":
-                print("Wpisz nazwę folderu:")
-                next_ = True
-                while next_:
-                    next_ = False
-                    inputMenu = input()
-                    for i in listDir:
-                        if inputMenu == i:
-                            next_ = True
-                            print("Taki folder istnieje")
-                            print("Wybierz inną nazwę")
-                    if not next_:
-                        print("Tworzę nowy folder: " + inputMenu)
-                        os.mkdir(os.getcwd() + chr(92) + inputMenu)
-                next_ = True
-
-            elif inputMenu == "-2":
-                next_ = True
-                next_2_ = True
-                while next_:
-                    print("Wybierz który folder ma mieć inną nazwę")
-                    listDir = os.listdir(os.getcwd())
-                    i = 1
-                    for var in listDir:
-                        print(str(i) + " --> " + str(var) + "\n")
-                        i = i+1
-                    print("0 --> Cofnij")
-                    inputMenu = input()
-                    if int(inputMenu) <= len(listDir) and int(inputMenu) > 0:
-                        print("Wybrano folder: " + str(listDir[int(inputMenu) - 1]))
-                    elif inputMenu == "0":
-                        next_ = False
-                        next_2_ = False
-                    else:
-                        clearCmd()
-                        print("Opcja: " + inputMenu + " nie istnieje, proszę wybrać ponownie\n")
-
-                while next_2_:
-                    print("Wpisz nazwę\n"
-                          "0 --> Cofnij")
-                    next_2_ = False
-                    inputName = input()
-                    if inputMenu == "0":
-                        return 0
-
-                    for i in listDir:
-                        if inputMenu == i:
-                            print("Taki folder już istnieje")
-                            next_2_ = True
-                            print("Wybierz inną nazwę")
-                    if not next_2_:
-                        print("Zmieniam nazwę folderu na: " + inputName)
-                        os.rename(listDir[int(inputMenu) - 1], inputName)
-                        next_2_ = False
-                else:
-                    print("Wybrałeś numer, który nie jest przypisany do folderu\n"
-                          "Wybierz folder lub cofnij")
-                next_ = True
-
-            elif inputMenu == "-3":
-                clearCmd()
-                next_ = True
-                while next_:
-                    print("Wybrano usunięcie folderu\n"
-                          "Aby usunąć folder, musi być pusty\n"
-                          "Wybierz folder\n")
-                    listDir = os.listdir(os.getcwd())
-                    i = 1
-                    for var in listDir:
-                        print(str(i) + " --> " + str(var) + "\n")
-                        i = i + 1
-                    print("0 --> Cofnij")
-                    inputMenu = input()
-                    if int(inputMenu) <= len(listDir) and int(inputMenu) > 0:
-                        print("Wybrano foder" + listDir[int(inputMenu) - 1])
-                        os.rmdir(os.getcwd() + chr(92) + listDir[int(inputMenu) - 1])
-                        next_ = False
-                    elif inputMenu == "0":
-                        next_ = False
-                    else:
-                        clearCmd()
-                        print("Opcja: " + inputMenu + " nie istnieje, proszę wybrać ponownie\n")
-                next_ = True
-
-            elif inputMenu == "0":
-                next_ = False
-        except ValueError:
-            print("Opcja: " + inputMenu + " nie istnieje, proszę wybrać ponownie\n")
->>>>>>> 1276d9168cde6062162e9d0560127d1660c0f87e
 
 
 
@@ -324,14 +214,9 @@ def intiger(input_):
 
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     path = os.getcwd()
     firstTest(path)
     menu(path)
-=======
-    firstTest()
-    menu()
->>>>>>> 1276d9168cde6062162e9d0560127d1660c0f87e
 
 
 
